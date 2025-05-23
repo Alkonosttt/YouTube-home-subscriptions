1d4f52d0e8326dd602708fdf2b6d76121fb7b898
Alkonosttt <voodoomon@protonmail.com>
2025-05-23 16:41:15 +0200

    Changed the script to make @match less strict

--- yt_home_to_sub_redirect.js
+++ yt_home_to_sub_redirect.js
@@ -1,24 +1,22 @@
 // ==UserScript==
 // @name         YouTube Homepage to Subscriptions Redirect
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Redirects from YouTube's homepage to your subscriptions
 // @author       Alkonosttt
-// @match        https://www.youtube.com/
-// @grant        window.location.replace
+// @match        https://www.youtube.com/*
 // @run-at       document-start
+// @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     const subscriptionsUrl = 'https://www.youtube.com/feed/subscriptions';
-    const currentUrl = window.location.href;
+    const homepageRegex = /^https:\/\/(www\.)?youtube\.com\/(\?[^\/]*)?$/;
 
-    const youtubeHomepageRegex = /^https:\/\/(www\.)?youtube\.com\/?(?:\?.*)?$/;
-
-    if (youtubeHomepageRegex.test(currentUrl)) {
-        console.log('Redirecting from YouTube homepage to subscriptions...');
+    if (homepageRegex.test(window.location.href)) {
+        console.log('Redirecting to subscriptions...');
         window.location.replace(subscriptionsUrl);
     }
 })();
